ce482aeaf1647e88d6ab5e81658e19c014a6e542
Carrysk <123112796@qq.com>
2019-01-02 23:08:35 +0800

    index

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http'
-/**
- * Generated class for the HomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,13 +9,16 @@ import { HttpClient } from '@angular/common/http'
 })
 export class HomePage {
 
-  index
+  carouselImgs = []
   constructor(public navCtrl: NavController, public navParams: NavParams, private myHttp: HttpClient) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
-    this.myHttp.get('http://127.0.0.1:3000')
+    this.myHttp.get('http://127.0.0.1:3000/index/getCarousel').subscribe((res: any)=>{
+      console.log(res)   // res: [{},{}...]
+      this.carouselImgs = res
+    })
   }
 
 }
